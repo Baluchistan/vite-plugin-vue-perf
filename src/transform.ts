2b8d33d1412ed169013b0a539f8ecf440536cfcf
yasir dora <143439570+Yasirdora@users.noreply.github.com>
2026-05-19 09:58:26 +0200

    feat: core AST traversal and performance mark injection
    Co-authored-by: Evan You <499550+yyx990803@users.noreply.github.com>

--- src/transform.ts
+++ src/transform.ts
@@ -1,10 +1,22 @@
 import MagicString from 'magic-string'
+import { parse, compileScript } from '@vue/compiler-sfc'
 
 export function transformVue(code: string, id: string, options: any) {
   const s = new MagicString(code)
+  const { descriptor } = parse(code)
   
-  // TODO: Implement AST parsing
+  if (!descriptor.scriptSetup && !descriptor.script) {
+    return null
+  }
   
+  const componentName = id.split('/').pop()?.replace('.vue', '') || 'Unknown'
+  
+  // Inject performance marks into onMounted and onUpdated
+  const injection = `\nimport { onMounted as _onMounted, onUpdated as _onUpdated } from 'vue'\n_onMounted(() => performance.mark('${componentName}-mounted'))\n_onUpdated(() => performance.mark('${componentName}-updated'))\n`
+  if (descriptor.scriptSetup) {
+    s.appendRight(descriptor.scriptSetup.loc.start.offset, injection)
+  }
+
   return {
     code: s.toString(),
     map: s.generateMap({ hires: true })
